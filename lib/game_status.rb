f630e410030c5284a30f425256d62ac001e5fc5c
Rida Kodad <kodadreda@gmail.com>
2019-08-07 11:07:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,7 +38,7 @@ end
       if board[position1] == "X" && board[position2] == "X" && board[position3] == "X" or ( board[position1] == "O" && board[position2] == "O" && board[position3] == "O" )
         return WIN_COMBINATIONS.fetch(nb4)
       else
-         false
+         boole(2)
       end
     nb4 += 1
   end
